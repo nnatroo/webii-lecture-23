4600f3d6beedf11792debfe470318734cf7e08a8
nnatroo <nugonatroshvili@gmail.com>
2024-12-18 19:55:13 +0400

    Add input for users

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import axios from "axios";
 
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState({})
+  const [inputData, setInputData] = useState('Tbilisi')
+  const [showError, setShowError] = useState(false)
 
   useEffect(() => {
-    axios.get(`http://api.weatherapi.com/v1/current.json?key=1b40634a79db4734baf203617210410&q=Tbilisi`)
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=1b40634a79db4734baf203617210410&q=${inputData}`)
       .then(function (response) {
         // handle success
         console.log(response.data);
@@ -15,14 +17,24 @@ const App = () => {
       .catch(function (error) {
         // handle error
         console.log(error);
+        setShowError(true);
+        setCurrentWeather({});
       })
       .finally(function () {
         // always executed
       });
-  }, []);
+  }, [inputData]);
+
+  const inputChangeHandler = (e) => {
+    setShowError(false);
+    setInputData(e.target.value);
+  }
 
   return (
     <>
+      <p>Enter city: </p>
+      <input value={inputData} onChange={inputChangeHandler} type="text"/>
+      {showError && <p>No data found!</p>}
       {currentWeather.current && currentWeather.location ? <>
         <h1>{`${currentWeather.location.name}, ${currentWeather.location.country}`}</h1>
         <h2>{currentWeather.current.temp_c} °C</h2>
